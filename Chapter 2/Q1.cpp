6e8bebdba7c44de37a646a4cfff5ec72727fce9e
Jed Gorman <jed.gorman@gmail.com>
2020-10-08 10:09:20 -0400

    Chapter 2!

---
+++ Chapter 2/Q1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+  const float pi = { 3.141592653589793238 };
+  float radius = { 0.0 };
+  float area = { 0.0 };
+  std::cout << "What is the radius of the circle?" << std::endl;
+  std::cin>> radius;
+
+  area = {pi * radius * radius};
+
+  std::cout << "The area of the circle is: " << radius << "units.\n" <<std::endl;
+
+  return 0;
+}
